69816da42223bb7ddd97016928c4a59dfd4f4ed6
Szymon Pituła <szymon.pitula@codeteam.pl>
2019-08-08 11:53:02 +0200

    Drobne czyszczenie startup.cs

--- Auth/Auth/Startup.cs
+++ Auth/Auth/Startup.cs
@@ -1,8 +1,6 @@
-﻿using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Auth;
 using Auth.Models;
-using Autofac.Integration.WebApi;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
@@ -19,19 +17,13 @@ namespace Auth
 
         public void Configuration(IAppBuilder app)
         {
-            DataProtectionProvider = app.GetDataProtectionProvider();
             // configure the db context, user manager and signin manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationUserManager>());
 
             UserManagerFactory = () => new UserManager<IdentityUser>(new UserStore<IdentityUser>());
 
-            var config = new HttpConfiguration
-            {
-                DependencyResolver = new AutofacWebApiDependencyResolver(AutoFacContainer.Container)
-            };
             app.UseAutofacMiddleware(AutoFacContainer.Container);
-            app.UseWebApi(config);
 
             ConfigureAuth(app);
         }
