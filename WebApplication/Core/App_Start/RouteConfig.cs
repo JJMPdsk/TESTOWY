a16a7a7291dceec1394b2474a47445c4b174075d
jjmp-dsk <szymon.pitula@codeteam.pl>
2020-01-10 18:26:42 +0100

    Włączenie tras customowych

--- WebApplication/Core/App_Start/RouteConfig.cs
+++ WebApplication/Core/App_Start/RouteConfig.cs
@@ -7,6 +7,7 @@ namespace Core
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapMvcAttributeRoutes();
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
